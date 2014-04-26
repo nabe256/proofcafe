a13c3a545e9734cd1401be71d086432106870376
WATANABE Yuichi <nabe.gm@gmail.com>
2014-04-26 15:07:07 +0900

    indent

--- 20140426/SsreflectSample.v
+++ 20140426/SsreflectSample.v
@@ -6,11 +6,11 @@ Variables A B C : Prop.
 
 Lemma HilbertS : (A -> B -> C) -> (A -> B) -> A -> C.
 Proof.
- move=> hAiBiC hAiB hA.
- move: hAiBiC.
- apply.
-   by [].
- by apply: hAiB.
+  move=> hAiBiC hAiB hA.
+  move: hAiBiC.
+  apply.
+    by [].
+  by apply: hAiB.
 Qed.
 
 End HilbertSaxiom.
