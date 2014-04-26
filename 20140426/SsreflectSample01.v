0ad86272df7e59ab3d4dd665d84883effa362d2e
WATANABE Yuichi <nabe.gm@gmail.com>
2014-04-26 17:23:53 +0900

    add andb_sym

--- 20140426/SsreflectSample01.v
+++ 20140426/SsreflectSample01.v
@@ -67,3 +67,38 @@ Proof.
 Qed.
 
 End HilbertSaxiom.
+
+Section Symmetric_Conjunction_Disjunction.
+
+Lemma andb_sym : forall A B : bool,
+  A && B -> B && A.
+Proof.
+  case.
+    by case.
+  by [].
+Qed.
+
+
+Lemma andb_sym' : forall A B : bool,
+  A && B -> B && A.
+Proof.
+  case.
+    case.
+      by [].
+    by case.
+  by [].
+Qed.
+
+Lemma andb_sym2 : forall A B : bool,
+  A && B -> B && A.
+Proof.
+  by case; case.
+Qed.
+
+Lemma andb_sym3 : forall A B : bool,
+  A && B -> B && A.
+Proof.
+  by do 2! case.
+Qed.
+
+End Symmetric_Conjunction_Disjunction.
